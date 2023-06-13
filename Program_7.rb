2ed9a9c101e39842803b7a6cf174b4c641335d5e
EMP0247 <hutish@vebuin.com>
2023-06-13 11:45:30 +0530

    "new changes

--- Program_7.rb
+++ Program_7.rb
@@ -1,10 +1,7 @@
 def find_words(word)
     flattened_words = word.flatten
-    sorted_word = flattened_words
-    sorted_word = sorted_word.sort_by(&:length)
-    long_words = sorted_word.last(3)
-    short_words = sorted_word.first(3)
-    [long_words,short_words]
+    flattened_words = flattened_words.sort_by(&:length)
+    [flattened_words.last(3),flattened_words.first(3)]
 end
 
 words = ['aaa','b',['bb','cccc'],'d',['e','ffff']]
